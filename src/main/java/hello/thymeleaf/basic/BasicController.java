7a1fdc84bd03c29cf39312e073c14d6c4aca707e
perfectoryinc <perfectoryinc@gmail.com>
2025-08-22 15:22:43 +0900

    feat: 타임리프 날짜 객체
    - 타임리프 날짜 객체 학습

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
 import java.util.*;
 
 @Controller
@@ -65,7 +66,15 @@ public class BasicController {
     static class HelloBean {
         public String hello(String data) {
             return "Hello " + data;
-        } }
+        }
+    }
+
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+
+        return "basic/date";
+    }
 
     @Data
     static class User {
