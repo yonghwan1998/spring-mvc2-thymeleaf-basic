af04d4c42ece71911de042431ba5962ec7a59ff0
perfectoryinc <perfectoryinc@gmail.com>
2025-08-22 15:44:51 +0900

    feat: 타임리프 literal
    - 타임리프 literal 학습

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -84,6 +84,14 @@ public class BasicController {
         return "basic/link";
     }
 
+    @GetMapping("/literal")
+    public String literal(Model model) {
+        model.addAttribute("data", "Spring!");
+        model.addAttribute("", "data2");
+
+        return "basic/literal";
+    }
+
     @Data
     static class User {
         private String username;
