27e2d1db9fca42f7d9b54ba489cb096b72c4c220
yongss <bbl737898@gmail.com>
2025-08-25 16:33:28 +0900

    feat: 타임리프 inline
    - th:inline을 사용하여 타임리프 데이터를 자바스크립트에서 인라인으로 사용 가능
    - 객체를 자동으로 JSON으로 바꿔줌

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -133,6 +133,14 @@ public class BasicController {
         return "basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("UserA", 10));
+        addUsers(model);
+
+        return "basic/javascript";
+    }
+
     private void addUsers(Model model) {
         ArrayList<User> list = new ArrayList<>();
         list.add(new User("userA", 10));
