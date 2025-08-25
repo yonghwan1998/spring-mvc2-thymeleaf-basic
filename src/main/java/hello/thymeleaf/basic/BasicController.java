4c53d461f5a5b6f07e596a853625da22db9f5330
yongss <bbl737898@gmail.com>
2025-08-25 16:08:32 +0900

    feat: 타임리프 if

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -112,6 +112,13 @@ public class BasicController {
         return "basic/each";
     }
 
+    @GetMapping("/condition")
+    public String condition(Model model) {
+        addUsers(model);
+
+        return "basic/condition";
+    }
+
     private void addUsers(Model model) {
         ArrayList<User> list = new ArrayList<>();
         list.add(new User("userA", 10));
